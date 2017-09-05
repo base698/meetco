503a1a7d3a558044b1d26beff5a11388ee59bce6
Justin Thomas <justin.thomas1@gmail.com>
2017-09-05 08:02:06 -0700

    rpcapi

--- create-chain.sh
+++ create-chain.sh
@@ -1,3 +1,3 @@
 geth --datadir .meetchain init genesis.json
 # geth --datadir ./.meetchain/ --networkid 15 --rpc --rpccorsdomain "*" console
-
+# geth --datadir ./.meetchain/ --networkid 15 --rpcapi="db,eth,net,personal,web3" --rpc --rpccorsdomain "*" console
